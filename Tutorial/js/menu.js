adab3453c7987a2385783a3af7bc8122a0c7d5f0
JonasF84 <jonas-fischer84@gmx.de>
2015-09-02 17:29:36 +0200

    Tutorial updated

---
+++ Tutorial/js/menu.js
@@ -0,0 +1,12 @@
+var createMenu = function(){
+    document.write("<div id=\"menu\">");
+    document.write("<nav>");
+    document.write("<ul>");
+    document.write("<li><a href=\"index.html\">Home</a></li>");
+    document.write("<li><a href=\"characters.html\">Characters</a></li>");
+    document.write("<li><a href=\"story.html\">Story</a></li>");
+    document.write("<li><a href=\"gear.html\">Gear</a></li>");
+    document.write("</ul>");
+    document.write("</nav>");
+    document.write("</div>");
+}
