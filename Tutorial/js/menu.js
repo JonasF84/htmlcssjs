f31dcb812c8630462184172cc7e693051fe80d1f
JonasF84 <jonas-fischer84@gmx.de>
2015-09-03 17:50:05 +0200

    updated and added stuff, bootstrap first commit

--- Tutorial/js/menu.js
+++ Tutorial/js/menu.js
@@ -1,4 +1,5 @@
 var createMenu = function(){
+    document.write("<div class=\"menulogocontainer\">");
     document.write("<div id=\"menu\">");
     document.write("<nav>");
     document.write("<ul>");
@@ -6,7 +7,13 @@ var createMenu = function(){
     document.write("<li><a href=\"characters.html\">Characters</a></li>");
     document.write("<li><a href=\"story.html\">Story</a></li>");
     document.write("<li><a href=\"gear.html\">Gear</a></li>");
+    document.write("<li><a href=\"hq.html\">HQ</a></li>");
+    document.write("<li><a href=\"poi.html\">POI</a></li>");
+    document.write("<li><a href=\"city.html\">The City</a></li>");
+    document.write("<li><a href=\"diceroller.html\">Dice Roller</a></li>");
     document.write("</ul>");
     document.write("</nav>");
+    document.write("</div>");
+    document.write("<img src=\"../img/logo2.png\" width=\"738\" height=\"276\">");
     document.write("</div>");
 }
